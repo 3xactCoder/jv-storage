6e64b38a095c15b94f5a403282e5e0dce21477b3
3xacT <vovaoliynyk99@gmail.com>
2026-02-05 17:20:30 +0200

    done generics task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,55 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private Object[] keys = new Object[MAX_SIZE];
+    private Object[] values = new Object[MAX_SIZE];
+    private int count = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if (key == null ? keys[i] == null : key.equals(keys[i])) {
+                values[i] = value; // перезапис значення
+                return;
+            }
+        }
+
+        for (int i = 0; i < count;i++) {
+            if (keys[i] == key) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        if (count < MAX_SIZE) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        } else {
+            throw new RuntimeException("Storage is already full");
+        }
+
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    return (V) values[i];
+                }
+            } else {
+                if (key.equals(keys[i])) {
+                    return (V) values[i];
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
